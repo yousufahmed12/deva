d500a68f4dd06f0dee6084a9dd00bd6845a7e367
yousufahmed12 <thuinahmed@yahoo.com>
2018-04-03 23:31:59 -0400

    Add files via upload

--- test/api.php
+++ test/api.php
@@ -5,26 +5,91 @@ include 'func.php';
 if ($_SERVER['REQUEST_METHOD'] == "GET"){
 	
 	if(isset($_GET['table']) && isset($_GET['id'])){
+		/***
+		*This will get all the a specfic user info
+		*
+		*The url will be (table = user& id = 'the users id')
+		*
+		*Input is the user table and the user id
+		*Output is a json with the user information
+		***/
 		if($_GET['table'] == "user"){
 			$table = $_GET['table'];
 			$id = $_GET['id'];
-			echo func9($id);
+			echo getUser($id);
 			http_response_code(200);
 		}
 		
 	}
 	else if(isset($_GET['table'])){
+		/***
+		*This will get all the data inside the table that is chosen
+		*
+		*The url will be (table = 'name of the table')
+		*
+		*Input is the name of the table
+		*Output is a json of all the data inside the table
+		***/
 		if($_GET['table']){
 			
 			$table = $_GET['table'];
-			echo func5($table);
+			echo getTable($table);
 			http_response_code(200);
 		}
 	}
+	else if(isset($_GET['starttime']) && isset($_GET['endtime']) && isset($_GET['type'])){
+		/***
+		*This will get all the lots that a user is allowed to reserve
+		*
+		*The url will be (starttime ='epoch input'&endtime='epoch input'&type='type of user)
+		*
+		*Input two epochs for the starttime and endtime and the user type
+		*Output is a json with the list of parking lot that the user can reserve
+		***/
+		$starttime = $_GET['starttime'];
+		$endtime = $_GET['endtime'];
+		$type = $_GET['type'];
+		
+		$StartTime = date(" H:i:s", substr($starttime, 0, 10));
+		$EndTime = date(" H:i:s", substr($endtime, 0, 10));
+		$Date = date("Y-m-d", substr($starttime, 0, 10));
+		
+		echo getAvailableWithType($StartTime,$EndTime,$Date,$type);
+		http_response_code(200);
+	}
+	else if(isset($_GET['starttime']) && isset($_GET['endtime'])){
+		/***
+		*This will get the lots and their availability
+		*
+		*The url will be (starttime ='epoch input'&endtime='epoch input')
+		*
+		*Input two epochs for the starttime and endtime
+		*Output is a json with the list of parking lot with the available spots
+		***/
+		$starttime = $_GET['starttime'];
+		$endtime = $_GET['endtime'];
+		
+		$StartTime = date(" H:i:s", substr($starttime, 0, 10));
+		$EndTime = date(" H:i:s", substr($endtime, 0, 10));
+		$Date = date("Y-m-d", substr($starttime, 0, 10));
+		
+		echo getAvailable($StartTime,$EndTime,$Date);
+		http_response_code(200);
+	}
+		
+	
 
 }
 else if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	if(isset($_GET['table'])){
+		/***
+		*This will get the add a new user to the database
+		*
+		*The url will be (table = user)
+		*
+		*Input will be a post body with the user info
+		*Output will be a boolean of true or false
+		***/
 		if($_GET['table'] == "user"){
 			$postBody = file_get_contents("php://input");
 			$postBody = json_decode($postBody);
@@ -43,18 +108,26 @@ else if ($_SERVER['REQUEST_METHOD'] == "POST"){
 			$isDisable = $postBody->$isDisable;
 			$status = $postBody->$status;
 			
-			echo func8($name, $email, $username, $password, $isDisable, $status);
+			echo postUser($name, $email, $username, $password, $isDisable, $status);
 			http_response_code(200);
 		}
 	} 
 }
 else if ($_SERVER['REQUEST_METHOD'] == "PUT"){
 	if(isset($_GET['table']) && isset($_GET['id']) && isset($_GET['newName'])){
+		/***
+		*This will update the name of a specfic user to a new name
+		*
+		*The url will be (table = user & id = 'specfic user id' & newName = 'the new name you want to add')
+		*
+		*Input will be the user id and the new name.
+		*Output will be a boolean of true or false
+		***/
 		if($_GET['table'] == "user"){
 			$table = $_GET['table'];
 			$id = $_GET['id'];
 			$newName = $_GET['newName'];
-			echo func10($id,$newName);
+			echo putName($id,$newName);
 			http_response_code(200);
 		}
 		
@@ -62,15 +135,31 @@ else if ($_SERVER['REQUEST_METHOD'] == "PUT"){
 }
 else if ($_SERVER['REQUEST_METHOD'] == "DELETE"){
 	if(isset($_GET['table']) && isset($_GET['id'])){
+		/***
+		*This will get the delete a user in the database
+		*
+		*The url will be (table = user & id = 'specfic user id')
+		*
+		*Input will the user id
+		*Output will be a boolean of true or false
+		***/
 		if($_GET['table'] == "user"){
 			$id = $_GET['id'];
-			echo func11($id);
+			echo deleteUser($id);
 			http_response_code(200);
 		}
 		
 	}
 }
+else if($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+        //added an options verb
+        //this allows the server to complete its steps for CORS
+        // previously 405 occured because it first looks for OPTIONS then compl$
+        echo 'options here';
+  }
 else {
-        http_response_code(405);
+         http_response_code(405);
 }
+
+
 ?>
--- test/func.php
+++ test/func.php
@@ -1,6 +1,11 @@
 <?php
-	
-function func5($table){
+/***
+*This will get the table data
+*
+*Input is the table name
+*Output will be the table data in json format
+***/	
+function getTable($table){
 	include 'config.php';
 	$table3 = "users";
 	$sql = "SELECT * FROM $table";
@@ -13,10 +18,15 @@ function func5($table){
 	echo json_encode($result_array);
 	}
 	
-		
-function func8($name, $email,$username,$password, $isDisable,$status){
+/***
+*This will add a user to the user table
+*
+*Input is the name, email,username,password,isDisable which is tinybit, status which is tinybit
+*Output will boolean of true or false if succesful or not
+***/	
+function postUser($name, $email,$username,$password, $isDisable,$status){
 	include 'config.php';
-	$sql = "INSERT INTO `user` (`UserID`, `PermitID`, `UserTypeID`, `Name`, `Email`, `Username`, `Password`, `isDisabled`, `Status`) 
+	$sql = "INSERT INTO `user` (`UserID`, `PermitID`, `UserTypeID`, `Name`, `Email`, `Username`, `Token`, `isDisabled`, `Status`) 
 	VALUES (NULL, NULL, NULL, '$name', '$email', '$username', '$password', '$isDisable', '$status')";
 	if ($mysqli->query($sql)==true){
 		echo "true";
@@ -25,8 +35,14 @@ function func8($name, $email,$username,$password, $isDisable,$status){
 	echo "false";
 	}
 }
-		
-function func9($id){
+
+/***
+*This will get a spefic user from the user table
+*
+*Input is the userid
+*Output will be the user info in a json format
+***/	
+function getUser($id){
 	include 'config.php';
 	$sql = "SELECT * FROM user WHERE UserID = $id";
 	$result = $mysqli->query($sql);
@@ -38,8 +54,13 @@ function func9($id){
 		echo json_encode($result_array);
 }
 		
-		
-function func10($id,$newName){
+/***
+*This will update a spefic users name
+*
+*Input is the userid and the new name
+*Output will be the updated user info in a json format
+***/	
+function putName($id,$newName){
 	include 'config.php';
 	$sql = "UPDATE webservice.user SET Name = '$newName' WHERE UserID = $id";
 		if ($mysqli->query($sql)==true){
@@ -56,8 +77,14 @@ function func10($id,$newName){
 	echo "false";
 	}
 }
-	
-function func11($id){
+
+/***
+*This will delete a spefic user from the user table
+*
+*Input is the userid
+*Output will boolean of true or false if succesful or not
+***/
+function deleteUser($id){
 	include 'config.php';
 	$sql = "DELETE FROM user WHERE UserID = $id";
 	if ($mysqli->query($sql)==true){
@@ -67,4 +94,128 @@ function func11($id){
 	echo "false";
 	}
 }
+
+
+/***
+*This will find the number available spots for each lot at specfic time and date
+*
+*Input is the starttime, endtime and the date
+*Output will be lotid, lotname, lotstatus, the total which is the number of avaliable spots in json 
+*with multiple lists of each lot with those specific information in the json
+*
+*Furthor notes this function will not work if the starttime is lesser then the endtime
+*Also time such as 1 pm is 13:00:00 while 2:00 am is 2:00:00
+*
+*The subquery clause for total has a combination of comparison to the time
+*The Starttime and Endtime for the reservation I used were 8:00 to 12:00
+*I needed four cases to test
+*The starttime and endtime input is inbetween 8:00 to 12:00
+*The starttime before 8:00 and and endtime inbetween 8:00 to 12:00
+*The starttime inbetween 8:00 to 12:00 and and after 12:00
+*The starttime after 8:00 and before 12:00
+*
+*
+***/
+function getAvailable($StartTime,$EndTime,$Date){
+    include 'config.php';
+     try {
+    $sql = "SELECT LotID,LotName,LotStatus, (MaxCapacity - DisabledSpots - ReservationSpots - 
+	(SELECT count(ReservationID) 
+	FROM parkinglot as p2
+	LEFT JOIN reservation as r2 USING (LotID)
+	WHERE (((('$StartTime' >= StartTime) AND ('$StartTime' < EndTime)) AND
+           (('$EndTime' > StartTime) AND ('$EndTime' <= EndTime))) OR
+           (('$StartTime' <= StartTime) AND ('$EndTime' > StartTime)) OR
+           (('$StartTime' < EndTime) AND ('$EndTime' >= EndTime))) AND Date = '$Date' AND
+	p2.LotID = p.LotID))
+	as total 
+	FROM parkinglot as p
+	LEFT JOIN reservation as r USING (LotID)
+	GROUP BY p.LotID";
+    $result = $mysqli->query($sql);
+
+    $result_array = array();
+        while($row = $result->fetch_assoc()){
+            $result_array[] = $row;
+        }
+        echo json_encode($result_array);
+         }
+    catch(PDOException $e)
+    {
+        die ('PDO error in getReservation()": ' . $e->getMessage() );
+    }
+}
+
+/***
+*This will display the lots that the user can reserve for, depending on the usertypeid and the times chosen
+*
+*Input is the starttime, endtime, date and usertypeid
+*Output will be lotid, lotname, lotstatus, the total which is the number of avaliable spots in json format
+*
+*Furthor notes this function will not work if the starttime is lesser then the endtime
+*Also time such as 1 pm is 13:00:00 while 2:00 am is 2:00:00
+*
+*The subquery clause for total has a combination of comparison to the time
+*The Starttime and Endtime for the reservation I used were 8:00 to 12:00
+*I needed four cases to test
+*The starttime and endtime input is inbetween 8:00 to 12:00
+*The starttime before 8:00 and and endtime inbetween 8:00 to 12:00
+*The starttime inbetween 8:00 to 12:00 and and after 12:00
+*The starttime after 8:00 and before 12:00
+*
+*In the subquery clause in the where clauaw will list the LotID that are valid with usertypeID and starttime and endtime
+*The cases check when the starttime is less then the endtime I will use 10:00 to 12:00 as the schedule start and end times
+*After the THEN it will check the input times with the schedule times, the schedule time being true such as 10:00 to 12:00 
+*it will first check to see if the start  and end tims input is NOT between 10:00 to 12:00
+*Using NOT between makes 9:00 to 10:00 false and 12:00 to 13:00 false(Which I don't want)
+*To make them true I use two OR to check for spefic conditions
+*I then And everything and check to see if 9:00 to 13:00 is invalid
+*After the THEN it is the ELSE clause which would check if the endtime is greater then the starttime
+*Like 6:00 to 2:00
+*It mirrors the THEN clause accept the Endtime and Starttime are switched
+***/
+function getAvailableWithType($StartTime,$EndTime,$Date,$Type){
+    include 'config.php';
+     try {
+    $sql = "SELECT LotID,LotName,LotStatus, (MaxCapacity - DisabledSpots - ReservationSpots - 
+	(SELECT count(ReservationID) 
+	FROM parkinglot as p2
+	LEFT JOIN reservation as r2 USING (LotID)
+	WHERE (((('$StartTime' >= StartTime) AND ('$StartTime' < EndTime)) AND
+           (('$EndTime' > StartTime) AND ('$EndTime' <= EndTime))) OR
+           (('$StartTime' <= StartTime) AND ('$EndTime' > StartTime)) OR
+           (('$StartTime' < EndTime) AND ('$EndTime' >= EndTime))) AND Date = '$Date' AND
+	p2.LotID = p.LotID))
+	as total 
+	FROM parkinglot as p
+	LEFT JOIN reservation as r USING (LotID)
+    WHERE LotID in (SELECT LotID
+	FROM schedule as s
+	WHERE s.UserTypeID = '$Type' AND
+	(CASE WHEN StartTime <  EndTime THEN 
+   (((('$StartTime' NOT BETWEEN StartTime AND EndTime) AND ('$EndTime' NOT BETWEEN StartTime AND EndTime)) OR
+      ('$StartTime' <= StartTime AND '$EndTime' = StartTime) OR 
+      ('$StartTime' = EndTime AND '$EndTime' >= EndTime)) AND
+      ('$StartTime' > StartTime OR '$EndTime' < EndTime))
+    ELSE
+   (((('$StartTime' NOT BETWEEN EndTime AND StartTime) AND ('$EndTime' NOT BETWEEN EndTime AND StartTime))  OR
+      ('$StartTime' <= EndTime AND '$EndTime' = EndTime) OR 
+      ('$StartTime' = StartTime AND '$EndTime' >= StartTime)) AND
+      ('$StartTime' > EndTime OR '$EndTime' < StartTime))
+	 END))
+	GROUP BY p.LotID";
+    $result = $mysqli->query($sql);
+
+    $result_array = array();
+        while($row = $result->fetch_assoc()){
+            $result_array[] = $row;
+        }
+        echo json_encode($result_array);
+         }
+    catch(PDOException $e)
+    {
+        die ('PDO error in getReservation()": ' . $e->getMessage() );
+    }
+}
+
 ?>
