d1998d122fe39f795c9a6326a5238fbbf45fab1c
yeunga46 <yeunga46@students.rowan.edu>
2018-04-19 19:11:52 -0400

    Update api.php

--- test/api.php
+++ test/api.php
@@ -136,6 +136,93 @@ else if ($_SERVER['REQUEST_METHOD'] == "POST"){
 			echo newComplaint($id, $report);
 			http_response_code(200);
 		}
+		/***
+		*This will get the add a new reservation to the database
+		*
+		*The url will be (table = reservation)
+		*
+		*Input will be a post body with the reservation info
+		*Output will be a boolean of true or false
+		***/
+		if($_GET['table'] == "reservation"){
+			$postBody = file_get_contents("php://input");
+			$postBody = json_decode($postBody);
+			
+			$lotid = "LotID";
+			$userid = "UserID";
+			$starttime = "StartTime";
+			$endtime = "EndTime";
+			
+			$lotid = $postBody->$lotid;
+			$userid = $postBody->$userid;
+			$starttime = $postBody->$starttime;
+			$endtime = $postBody->$endtime;
+
+			$StartTime = date(" H:i:s", substr($starttime, 0, 10));
+			$EndTime = date(" H:i:s", substr($endtime, 0, 10));
+			$Date = date("Y-m-d", substr($starttime, 0, 10));
+
+			echo newReservation($lotid, $userid, $Date,$StartTime, $EndTime);
+			http_response_code(200);
+		}
+		/***
+		*This will get the add a new lot to the database
+		*
+		*The url will be (table = parkinglot)
+		*
+		*Input will be a post body with the lot info
+		*Output will be a boolean of true or false
+		***/
+		if($_GET['table'] == "parkinglot"){
+			$postBody = file_get_contents("php://input");
+			$postBody = json_decode($postBody);
+			
+			$lotname= "LotName";
+			$max = "MaxCapacity";
+			$dspots = "DisabledSpots";
+			$rspots = "ReservationSpots";
+			$lstatus = "LotStatus";
+			$reservable = "isReservable";
+			
+			$lotname = $postBody->$lotname;
+			$max = $postBody->$max;
+			$dspots = $postBody->$dspots;
+			$rspots = $postBody->$rspots;
+			$lstatus = $postBody->$lstatus;
+			$reservable = $postBody->$reservable;
+
+			echo newLot($lotname, $max, $dspots, $rspots, $lstatus, $reservable);
+			http_response_code(200);
+		}
+		/***
+		*This will get the add a new schedule to the database
+		*
+		*The url will be (table = schedule)
+		*
+		*Input will be a post body with the schedule info
+		*Permit id: 1 Commuter, 2 Resident 3 Employee 
+		*Output will be a boolean of true or false
+		***/
+		if($_GET['table'] == "schedule"){
+			$postBody = file_get_contents("php://input");
+			$postBody = json_decode($postBody);
+			
+			$lotid = "LotID";
+			$pid = "PermitID";
+			$starttime = "StartTime";
+			$endtime = "EndTime";
+			
+			$lotid = $postBody->$lotid;
+			$pid = $postBody->$pid;
+			$starttime = $postBody->$starttime;
+			$endtime = $postBody->$endtime;
+
+			$StartTime = date(" H:i:s", substr($starttime, 0, 10));
+			$EndTime = date(" H:i:s", substr($endtime, 0, 10));
+
+			echo newSchedule($lotid, $pid, $StartTime, $EndTime);
+			http_response_code(200);
+		}
 	} 
 }
 else if ($_SERVER['REQUEST_METHOD'] == "PUT"){
@@ -153,6 +240,42 @@ else if ($_SERVER['REQUEST_METHOD'] == "PUT"){
 			$id = $_GET['id'];
 			$newName = $_GET['newName'];
 			echo putName($id,$newName);
+			http_response_code(200);
+		}
+		
+	}
+	else if(isset($_GET['function'])&& isset($_GET['id']) && isset($_GET['m']) && isset($_GET['d']) && isset($_GET['r'])) {
+		/***
+		*This will update a lot  max capacity , number of disabled spots, and number of reservation spots
+		*
+		*The url will be (function = updateLot & id = 'specfic lot id' & m = 'new max capacicity' & d = 'new # of disabled spots' & r = 'new # of reservation spots' )
+		*
+		*Input will be the lot id,max capacity , number of disabled spots, and number of reservation spots
+		*Output will be a boolean of true or false
+		***/
+		if($_GET['function'] == "updateLot"){
+			$id = $_GET['id'];
+			$max = $_GET['m'];
+			$dspots = $_GET['d'];
+			$rspots = $_GET['r'];
+	
+			echo updateLot($id, $max, $dspots, $rspots);
+			http_response_code(200);
+		}
+	}
+	else if(isset($_GET['function']) && isset($_GET['id']) && isset($_GET['lotid'])){
+		/***
+		*This will set a schedule to a specfic lot
+		*
+		*The url will be (function = changeLotSchedule & id = 'specfic schedule id' & lotid = 'the new lot you want to add the schedule to')
+		*
+		*Input will be the schedule id and the lot id.
+		*Output will be a boolean of true or false
+		***/
+		if($_GET['function'] == "changeLotSchedule"){
+			$lotid = $_GET['lotid'];
+			$id = $_GET['id'];
+			echo changeLotSchedule($lotid, $id);
 			http_response_code(200);
 		}
 		
@@ -236,7 +359,23 @@ else if ($_SERVER['REQUEST_METHOD'] == "PUT"){
 			echo closeLot($id);
 			http_response_code(200);
 		}
+		/***
+		*This will update the ReservationStatus to be 0 or in boolean false
+		*
+		*The url will be (function = cancelReservation & id = 'specfic reservation id' )
+		*
+		*Input will be the Reservation id
+		*Output will be a boolean of true or false
+		***/
+		else if($_GET['function'] == "cancelReservation"){
+			$id = $_GET['id'];
+			echo cancelReservation($id);
+			http_response_code(200);
+		}
 	}
+	
+	 
+	
 }
 else if ($_SERVER['REQUEST_METHOD'] == "DELETE"){
 	if(isset($_GET['table']) && isset($_GET['id'])){
@@ -256,15 +395,31 @@ else if ($_SERVER['REQUEST_METHOD'] == "DELETE"){
 		/***
 		*This will get the delete a complaint in the database
 		*
-		*The url will be (table = complaints & id = 'specfic user id')
+		*The url will be (table = complaints & id = 'specfic complaint id')
 		*
 		*Input will the complaints id
 		*Output will be a boolean of true or false
 		***/
-		else if($_GET['table'] == "complaints"){
+		if($_GET['table'] == "complaints"){
 			
 			$id = $_GET['id'];
 			echo removeComplaint($id);
+			http_response_code(200);
+			
+			
+		} 
+		/***
+		*This will get the delete a lot in the database
+		*
+		*The url will be (table = parkinglot & id = 'specfic lot id')
+		*
+		*Input will the complaints id
+		*Output will be a boolean of true or false
+		***/
+		if($_GET['table'] == "parkinglot"){
+			
+			$id = $_GET['id'];
+			echo removeLot($id);
 			http_response_code(200);
 			
 			
