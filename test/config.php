436307ca6542c19df40931dde08a43ce00399212
yeunga46 <yeunga46@students.rowan.edu>
2018-04-21 16:13:37 -0400

    Update config.php

--- test/config.php
+++ test/config.php
@@ -1,10 +1,14 @@
  <?php
-$servername = "localhost";
-$user = "root";
-$passwd = "Aswe2018*";
-$dbname ="webservice";
+    $servername = "localhost";
+    $user = "phpmyadmin";
+    $passwd = "Aswe2018*";
+    $dbname ="webservice";
 
      $mysqli = mysqli_connect($servername,$user,$passwd,$dbname);//login to database
 	 
-
+	// Check connection
+	if (!$mysqli)
+	  {
+	  	die("Connection error: " . mysqli_connect_errno());
+	  }
 ?>
